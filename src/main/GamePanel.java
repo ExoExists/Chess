12bf6baa2ece9f98222eb3f6d2636c49f2453deb
ExoExists <274657144+ExoExists@users.noreply.github.com>
2026-08-06 16:54:36 -0400

    Final game

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -83,40 +83,40 @@ public class GamePanel extends JPanel implements Runnable{
 
     public void setPieces(){
         // White team
-  //      pieces.add(new Pawn(WHITE, 0,6));
-    //    pieces.add(new Pawn(WHITE, 1,6));
-      //  pieces.add(new Pawn(WHITE, 2,6));
-       // pieces.add(new Pawn(WHITE, 3,6));
-        //pieces.add(new Pawn(WHITE, 4,6));
-        //pieces.add(new Pawn(WHITE, 5,6));
-        //pieces.add(new Pawn(WHITE, 6,6));
-        //pieces.add(new Pawn(WHITE, 7,6));
-        //pieces.add(new Knight(WHITE, 1,7));
-        //pieces.add(new Knight(WHITE, 6,7));
-        //pieces.add(new Rook(WHITE, 7,7));
-        //pieces.add(new Rook(WHITE, 0,7));
-        //pieces.add(new Bishop(WHITE, 2,7));
-        //pieces.add(new Bishop(WHITE, 5,7));
+        pieces.add(new Pawn(WHITE, 0,6));
+        pieces.add(new Pawn(WHITE, 1,6));
+        pieces.add(new Pawn(WHITE, 2,6));
+        pieces.add(new Pawn(WHITE, 3,6));
+        pieces.add(new Pawn(WHITE, 4,6));
+        pieces.add(new Pawn(WHITE, 5,6));
+        pieces.add(new Pawn(WHITE, 6,6));
+        pieces.add(new Pawn(WHITE, 7,6));
+        pieces.add(new Knight(WHITE, 1,7));
+        pieces.add(new Knight(WHITE, 6,7));
+        pieces.add(new Rook(WHITE, 7,7));
+        pieces.add(new Rook(WHITE, 0,7));
+        pieces.add(new Bishop(WHITE, 2,7));
+        pieces.add(new Bishop(WHITE, 5,7));
         pieces.add(new Queen(WHITE, 3,7));
         pieces.add(new King(WHITE, 4,7));
 
         // Black team
-    //    pieces.add(new Pawn(BLACK, 0,1));
-    //    pieces.add(new Pawn(BLACK, 1,1));
-    //    pieces.add(new Pawn(BLACK, 2,1));
-   //     pieces.add(new Pawn(BLACK, 3,1));
-   //     pieces.add(new Pawn(BLACK, 4,1));
-    //    pieces.add(new Pawn(BLACK, 5,1));
-   //     pieces.add(new Pawn(BLACK, 6,1));
-   //     pieces.add(new Pawn(BLACK, 7,1));
-   //     pieces.add(new Knight(BLACK, 1,0));
-   //     pieces.add(new Knight(BLACK, 6,0));
-   //     pieces.add(new Rook(BLACK, 0,0));
-   //     pieces.add(new Rook(BLACK, 7,0));
-    //    pieces.add(new Bishop(BLACK, 2,0));
-    //    pieces.add(new Bishop(BLACK, 5,0));
-     //   pieces.add(new Queen(BLACK, 3,0));
-       pieces.add(new King(BLACK, 4,0));
+        pieces.add(new Pawn(BLACK, 0,1));
+        pieces.add(new Pawn(BLACK, 1,1));
+        pieces.add(new Pawn(BLACK, 2,1));
+        pieces.add(new Pawn(BLACK, 3,1));
+        pieces.add(new Pawn(BLACK, 4,1));
+        pieces.add(new Pawn(BLACK, 5,1));
+        pieces.add(new Pawn(BLACK, 6,1));
+        pieces.add(new Pawn(BLACK, 7,1));
+        pieces.add(new Knight(BLACK, 1,0));
+        pieces.add(new Knight(BLACK, 6,0));
+        pieces.add(new Rook(BLACK, 0,0));
+        pieces.add(new Rook(BLACK, 7,0));
+        pieces.add(new Bishop(BLACK, 2,0));
+        pieces.add(new Bishop(BLACK, 5,0));
+        pieces.add(new Queen(BLACK, 3,0));
+        pieces.add(new King(BLACK, 4,0));
         
     }
     private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target){
@@ -163,7 +163,7 @@ public class GamePanel extends JPanel implements Runnable{
                                     // TODO : POSSIBLE GAME OVER
                                     gameover = true;
                                 }
-                                else if(isStalemate() && isKingInCheck()==false){
+                                else if(isStalemate() && isKingInCheck() == false){
                                     stalemate = true;
                                 }
                                  else{ //continue game
@@ -308,7 +308,11 @@ public class GamePanel extends JPanel implements Runnable{
         for(Piece p : simPieces){
             p.draw(graphics2d);
         }
+                graphics2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+                graphics2d.setFont(new Font("Book Antiqua", Font.PLAIN, 40));
+                graphics2d.setColor(Color.white);
         if(activeP != null){
+            
             if(canMove){
                 if(isIllegal(activeP) && opponentCanCaptureKing()){
                     graphics2d.setColor(Color.gray);
