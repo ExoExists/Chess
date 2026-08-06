7a6d868d46bb1cd222528160ceb9aead3f40d735
ExoExists <274657144+ExoExists@users.noreply.github.com>
2026-08-06 16:50:02 -0400

    Stalemate

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -43,7 +43,7 @@ public class GamePanel extends JPanel implements Runnable{
     boolean validSquare;
     boolean promotion;
     boolean gameover;
-
+    boolean stalemate;
     public GamePanel(){
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.black);
@@ -83,40 +83,40 @@ public class GamePanel extends JPanel implements Runnable{
 
     public void setPieces(){
         // White team
-        pieces.add(new Pawn(WHITE, 0,6));
-        pieces.add(new Pawn(WHITE, 1,6));
-        pieces.add(new Pawn(WHITE, 2,6));
-        pieces.add(new Pawn(WHITE, 3,6));
-        pieces.add(new Pawn(WHITE, 4,6));
-        pieces.add(new Pawn(WHITE, 5,6));
-        pieces.add(new Pawn(WHITE, 6,6));
-        pieces.add(new Pawn(WHITE, 7,6));
-        pieces.add(new Knight(WHITE, 1,7));
-        pieces.add(new Knight(WHITE, 6,7));
-        pieces.add(new Rook(WHITE, 7,7));
-        pieces.add(new Rook(WHITE, 0,7));
-        pieces.add(new Bishop(WHITE, 2,7));
-        pieces.add(new Bishop(WHITE, 5,7));
+  //      pieces.add(new Pawn(WHITE, 0,6));
+    //    pieces.add(new Pawn(WHITE, 1,6));
+      //  pieces.add(new Pawn(WHITE, 2,6));
+       // pieces.add(new Pawn(WHITE, 3,6));
+        //pieces.add(new Pawn(WHITE, 4,6));
+        //pieces.add(new Pawn(WHITE, 5,6));
+        //pieces.add(new Pawn(WHITE, 6,6));
+        //pieces.add(new Pawn(WHITE, 7,6));
+        //pieces.add(new Knight(WHITE, 1,7));
+        //pieces.add(new Knight(WHITE, 6,7));
+        //pieces.add(new Rook(WHITE, 7,7));
+        //pieces.add(new Rook(WHITE, 0,7));
+        //pieces.add(new Bishop(WHITE, 2,7));
+        //pieces.add(new Bishop(WHITE, 5,7));
         pieces.add(new Queen(WHITE, 3,7));
         pieces.add(new King(WHITE, 4,7));
 
         // Black team
-        pieces.add(new Pawn(BLACK, 0,1));
-        pieces.add(new Pawn(BLACK, 1,1));
-        pieces.add(new Pawn(BLACK, 2,1));
-        pieces.add(new Pawn(BLACK, 3,1));
-        pieces.add(new Pawn(BLACK, 4,1));
-        pieces.add(new Pawn(BLACK, 5,1));
-        pieces.add(new Pawn(BLACK, 6,1));
-        pieces.add(new Pawn(BLACK, 7,1));
-        pieces.add(new Knight(BLACK, 1,0));
-        pieces.add(new Knight(BLACK, 6,0));
-        pieces.add(new Rook(BLACK, 0,0));
-        pieces.add(new Rook(BLACK, 7,0));
-        pieces.add(new Bishop(BLACK, 2,0));
-        pieces.add(new Bishop(BLACK, 5,0));
-        pieces.add(new Queen(BLACK, 3,0));
-        pieces.add(new King(BLACK, 4,0));
+    //    pieces.add(new Pawn(BLACK, 0,1));
+    //    pieces.add(new Pawn(BLACK, 1,1));
+    //    pieces.add(new Pawn(BLACK, 2,1));
+   //     pieces.add(new Pawn(BLACK, 3,1));
+   //     pieces.add(new Pawn(BLACK, 4,1));
+    //    pieces.add(new Pawn(BLACK, 5,1));
+   //     pieces.add(new Pawn(BLACK, 6,1));
+   //     pieces.add(new Pawn(BLACK, 7,1));
+   //     pieces.add(new Knight(BLACK, 1,0));
+   //     pieces.add(new Knight(BLACK, 6,0));
+   //     pieces.add(new Rook(BLACK, 0,0));
+   //     pieces.add(new Rook(BLACK, 7,0));
+    //    pieces.add(new Bishop(BLACK, 2,0));
+    //    pieces.add(new Bishop(BLACK, 5,0));
+     //   pieces.add(new Queen(BLACK, 3,0));
+       pieces.add(new King(BLACK, 4,0));
         
     }
     private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target){
@@ -129,7 +129,7 @@ public class GamePanel extends JPanel implements Runnable{
         if(promotion){
             promoting();
         }
-        else if(gameover == false){
+        else if(gameover == false && stalemate == false){
             if(mouse.pressed){
                         //if the acrtive P i snull, check if you can pick up a piece
                         if(activeP==null){
@@ -162,6 +162,9 @@ public class GamePanel extends JPanel implements Runnable{
                                 if(isKingInCheck() && isCheckmate()){
                                     // TODO : POSSIBLE GAME OVER
                                     gameover = true;
+                                }
+                                else if(isStalemate() && isKingInCheck()==false){
+                                    stalemate = true;
                                 }
                                  else{ //continue game
                                     if(canPromote()){
@@ -366,6 +369,13 @@ public class GamePanel extends JPanel implements Runnable{
             graphics2d.setColor(Color.green);
             graphics2d.drawString(a, 200, 420);
         }
+
+        if(stalemate){
+            graphics2d.setFont(new Font("Arial", Font.PLAIN,90));
+            graphics2d.setColor(Color.lightGray);
+            graphics2d.drawString("Stalemate", 200, 420);            
+
+        }
         
     }
     private void changePlayer(){
@@ -534,7 +544,22 @@ public class GamePanel extends JPanel implements Runnable{
         return isValidMove;
         
     }
-
+    private boolean isStalemate(){
+        int count = 0;
+        //count the number of pieces
+        for(Piece piece : simPieces){
+            if(piece.color !=currentColor){
+                count++;
+            }
+        }
+        //if only one piece(the king) is left
+        if(count ==1){
+            if(kingCanMove(getKing(true))==false){
+                return true;
+            }
+        }
+        return false;
+    }
     private void checkCastling(){
         if(castlingP != null){
             if(castlingP.col == 0){
